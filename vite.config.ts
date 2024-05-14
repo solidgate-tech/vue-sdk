20bb51006802d1c1946f35d7fd805a3881ed5b5a
Mykhailo Dziuba <mykhailo.dziuba@solidgate.com>
2024-05-14 11:11:41 +0200

    fix(SIG2-32703): fix styles not been applied in build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 import * as path from 'path'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), libInjectCss()],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'lib/index.ts'),
